20f24ba7771cb00bb31e550989ca99ee7f8ceb3b
shenlong95 <49504938+shenlong95@users.noreply.github.com>
2021-07-06 18:04:39 +0800

    Add files via upload

--- RDP.sh
+++ RDP.sh
@@ -3,7 +3,7 @@ printf "Installing RDP Be Patience... " >&2
 {
 sudo useradd -m SHEN
 sudo adduser SHEN sudo
-echo 'SHEN:8426' | sudo chpasswd
+echo 'SHEN:346800' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
